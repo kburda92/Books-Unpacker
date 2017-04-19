23e36b271c14369bd70e7381691b0159f3bcef5d
krzysiak <krzysiak1992@gmail.com>
2017-04-19 19:26:07 +0200

    added 2 listboxes and button to find all directories + config.ini in .gitignore

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from tkinter import  *
 from tkinter import filedialog
 from Config import Config
+from os import *
 
 class Application(Frame):
     def __init__(self, master=None):
@@ -12,19 +13,33 @@ class Application(Frame):
         master.protocol('WM_DELETE_WINDOW', self.__save_and_exit)
 
     def __create_widgets(self):
-        widgets = (("Source Folder", self.source_folder), ("Destination Folder", self.dest_folder))
+        entries = (("Source Folder", self.source_folder), ("Destination Folder", self.dest_folder))
         row = 0
         self.grid()
-        for widget in widgets:
-            text = Label(self, text=widget[0])
+        for entry in entries:
+            text = Label(self, text = entry[0])
             text.grid(row=row, column=0, padx=5, pady=5)
 
-            folder = Entry(self, width = 30, textvariable = widget[1])
-            folder.grid(row=row, column=1, columnspan=3, padx=5, pady=5)
+            folder = Entry(self, width = 30, textvariable = entry[1])
+            folder.grid(row=row, column=1, padx=5, pady=5)
 
-            button = Button(self, text="...", command=lambda value = widget[1]: self.__set_folder(value))
-            button.grid(row=row, column=4, padx=5, pady=5)
+            button = Button(self, text="...", command=lambda value = entry[1]: self.__set_folder(value))
+            button.grid(row=row, column=2, padx=5, pady=5)
             row += 1
+
+        self._search_button = Button(self, text="Search for books", command= self.__search_for_books)
+        self._search_button.grid(row=0, column=3, rowspan=2, padx=5, pady=5)
+
+        self.found_dirs = Listbox(self)
+        self.found_dirs.grid(row=3, column=1, sticky=E+W, padx=5, pady=5)
+        scrollbar = Scrollbar(self)
+        scrollbar.grid(row=3,column=1,sticky=N+S+E+W, padx=5, pady=5)
+        self.found_dirs.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=self.found_dirs.yview)
+
+        self.found_files = Listbox(self)
+        self.found_files.grid(row=3, column=2, columnspan=2, sticky=W+E, padx=5, pady=5)
+        # self.found_files.config(yscrollcommand=scrollbar.set)
 
     def __load_paths(self):
         self.config = Config()
@@ -32,7 +47,17 @@ class Application(Frame):
         self.dest_folder.set(self.config.dest_path)
 
     def __set_folder(self, path):
-        path.set(filedialog.askdirectory())
+        dir = filedialog.askdirectory()
+        if dir:
+             path.set(dir)
+
+    def __search_for_books(self):
+        i=0
+        directories = [d for d in listdir(self.source_folder.get()) if path.isdir(path.join(self.source_folder.get(),d))]
+        for dir in list(directories):
+            self.found_dirs.insert(i, dir)
+            i+=1
+        self._search_button.configure(state='disabled')
 
     def __save_and_exit(self):
         self.config.source_path = self.source_folder.get()
